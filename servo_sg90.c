6e84ed1a9a5402f29ef2df34d706085b9145f885
Serhii Kuzmychov <ku3cmich@gmail.com>
2024-02-03 20:19:15 +0200

    sync

--- servo_sg90.c
+++ servo_sg90.c
@@ -1,44 +1,21 @@
-#include <servo_mg90s.h>
+#include <servo_sg90.h>
 
-#ifndef SERVO_MG90S_DEBUG
-LOG_MODULE_REGISTER(SERVO_MG90S, LOG_LEVEL_INF);
+#ifndef SERVO_SG90_DEBUG
+LOG_MODULE_REGISTER(SERVO_SG90, LOG_LEVEL_INF);
 #else
-LOG_MODULE_REGISTER(SERVO_MG90S, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(SERVO_SG90, LOG_LEVEL_DBG);
 #endif
 
-int servo_mg90s_init(servo_mg90s_t *servo, const pwm_t *pwm)
+int servo_sg90_init(servo_sg90_t *servo, const pwm_t *pwm)
 {
     servo->pwm = pwm;
-    fallbacks(pwm_set_dt, servo->pwm, SERVO_FREQ, SERVO_POS_0);
-    k_event_init(&servo->tick);
-    k_timer_init(&servo->timer, servo_mg90s_tick, NULL);
-    (servo->timer).user_data = servo;
+    fallbacks(pwm_set_dt, servo->pwm, SERVO_SG90_FREQ, SERVO_SG90_POS_0);
 
     return 0;
 }
 
-void servo_mg90s_tick(k_timer_t *timer)
+int servo_sg90_rotate(servo_sg90_t *servo, int32_t pos])
 {
-    servo_mg90s_t *servo = timer->user_data;
-    k_event_set(&servo->tick, 1);
-}
-
-int servo_mg90s_rotate(servo_mg90s_t *servo, int32_t direction, size_t ticks)
-{
-    fallbacks(pwm_set_dt, servo->pwm, SERVO_FREQ, direction);
-    k_timer_start(&servo->timer, SERVO_TIMER_TICKS(ticks), K_NO_WAIT);
-    k_event_wait(&servo->tick, 1, true, K_FOREVER);
-    fallbacks(pwm_set_dt, servo->pwm, SERVO_FREQ, SERVO_POS_0);
-
+    fallbacks(pwm_set_dt, servo->pwm, SERVO_SG90_FREQ, pos);
     return 0;
-}
-
-int servo_mg90s_rotate_start(servo_mg90s_t *servo, int32_t direction)
-{
-    return pwm_set_dt(servo->pwm, SERVO_FREQ, direction);
-}
-
-int servo_mg90s_rotate_stop(servo_mg90s_t *servo, int32_t direction)
-{
-    return pwm_set_dt(servo->pwm, SERVO_FREQ, SERVO_POS_0);
 }
--- servo_sg90.h
+++ servo_sg90.h
@@ -1,29 +1,22 @@
-#ifndef __servo_mg_90s__
-#define __servo_mg_90s__
+#ifndef __servo_sg90__
+#define __servo_sg90__
 
 #include <xglob.h>
 #include <xlog.h>
 #include <zephyr/drivers/pwm.h>
 
-#define SERVO_FREQ_MSEC 10
-#define SERVO_FREQ PWM_MSEC(SERVO_FREQ_MSEC)
-#define SERVO_TIMER_TICKS(n) K_MSEC(n *SERVO_FREQ_MSEC)
+#define SERVO_SG90S_FREQ_MSEC 10
+#define SERVO_SG90S_FREQ PWM_MSEC(SERVO_SG90S_FREQ_MSEC)
+#define SERVO_SG90S_TIMER_TICKS(n) K_MSEC(n *SERVO_SG90S_FREQ_MSEC)
 
-#define SERVO_POS_0 PWM_USEC(1500)
-#define SERVO_CW PWM_USEC(1000)
-#define SERVO_CCW PWM_USEC(2000)
+#define SERVO_SG90S_POS_0 PWM_USEC(1500)
 
 typedef struct
 {
     const pwm_t *pwm;
-    k_event_t tick;
-    k_timer_t timer;
-} servo_mg90s_t;
+} servo_sg90_t;
 
-void servo_mg90s_tick(k_timer_t *timer);
-int servo_mg90s_init(servo_mg90s_t *servo, const pwm_t *pwm);
-int servo_mg90s_rotate(servo_mg90s_t *servo, int32_t direction, size_t ticks);
-int servo_mg90s_rotate_start(servo_mg90s_t *servo, int32_t direction);
-int servo_mg90s_rotate_stop(servo_mg90s_t *servo, int32_t direction);
+int servo_sg90_init(servo_sg90_t *servo, const pwm_t *pwm);
+int servo_sg90_rotate(servo_sg90_t *servo, int32_t direction, size_t pwm);
 
 #endif
